2ea131bf4e3389f06435d28547cb4cd27b08ed43
FelixPierre <44431537+FelixPierre@users.noreply.github.com>
2020-02-15 15:47:07 +0100

    project creation
    Unity 2019.3.0f6 URP project
    + use of new input system package

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    Vector2 direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Move(InputAction.CallbackContext context) {
+        direction = context.ReadValue<Vector2>();
+        Debug.Log("move: " + context);
+    }
+
+    public void Fire(InputAction.CallbackContext context) {
+        Debug.Log("fire " + context);
+    }
+}
