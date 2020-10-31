7224bd48e0a09cd107aca163ca097a3ea808552a
FelixPierre <44431537+FelixPierre@users.noreply.github.com>
2020-10-31 18:08:10 +0100

    base of ennemy ai

---
+++ Assets/Scripts/Enemy/Behavior/ChaseBehavior.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaseBehavior : StateMachineBehaviour
+{
+    [SerializeField] float searchTime = 5;
+
+    EnemyAI ai;
+    float stopSearchTime;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        ai = animator.GetComponent<EnemyAI>();
+        stopSearchTime = Time.time + searchTime;
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        if (stopSearchTime < Time.time) {
+            ai.Stop();
+            ai.target = null;
+            animator.SetBool("enemySpotted", false);
+        }
+        else {
+            if (ai.IsTargetVisible()) {
+                if (ai.IsTargetInRange()) {
+                    ai.Stop();
+                }
+                else {
+                    ai.MoveTo(ai.target.position);
+                }
+                stopSearchTime = Time.time + searchTime;
+            }
+        }
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
---
+++ Assets/Scripts/Enemy/EnemyAI.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] public float fireRange;
+    [SerializeField] public float visionRange;
+    [SerializeField] public Transform target;
+
+    [SerializeField] private NavMeshAgent m_agent;
+    [SerializeField] private Animator m_animator;
+    [SerializeField] private Transform m_firePoint;
+
+
+    /// <summary>
+    /// Move to position
+    /// </summary>
+    /// <param name="position"></param>
+    public void MoveTo(Vector3 position) {
+        m_agent.isStopped = false;
+        m_agent.SetDestination(position);
+    }
+
+
+    /// <summary>
+    /// Stop to move
+    /// </summary>
+    public void Stop() {
+        m_agent.isStopped = true;
+    }
+
+
+    /// <summary>
+    /// Aim the target
+    /// </summary>
+    /// <param name="target"></param>
+    public void Aim(Transform target) {
+
+    }
+
+    /// <summary>
+    /// Check if the target is in the vision range, with a direct view on it
+    /// </summary>
+    /// <returns>true if the target is in view, else false</returns>
+    public bool IsTargetVisible() {
+        return RaycastTarget(transform, target, visionRange);
+    }
+
+
+    /// <summary>
+    /// Check if the target is in the vision range, with a direct view on it
+    /// </summary>
+    /// <param name="target"></param>
+    /// <returns>true if the target is in view, else false</returns>
+    public bool IsTargetVisible(Transform target) {
+        return RaycastTarget(transform, target, visionRange);
+    }
+
+
+    /// <summary>
+    /// Check if the target is in the fire range, with a direct view on it
+    /// </summary>
+    /// <returns>true if the target is in range, else false</returns>
+    public bool IsTargetInRange() {
+        return RaycastTarget(m_firePoint, target, fireRange);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="target"></param>
+    /// <returns></returns>
+    public bool IsInFiringLine(Transform target) {
+        return RayCast(m_firePoint.position, m_firePoint.forward, fireRange, target);
+    }
+
+
+    private bool RaycastTarget(Transform src, Transform target, float range) {
+        Vector3 direction = Vector3.Normalize(target.position - src.position);
+
+        Debug.DrawLine(src.position, target.position, Color.red);
+        return RayCast(src.position, direction, range, target);
+    }
+
+    private bool RayCast(Vector3 origin, Vector3 direction, float range, Transform target) {
+        if (Physics.Raycast(origin, direction, out RaycastHit hit, range)) {
+            return hit.collider.gameObject == target.gameObject;
+        }
+        return false;
+    }
+}
