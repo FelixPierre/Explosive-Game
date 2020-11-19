3655329e35cc30183470bda53bf6b698a7c9d717
FelixPierre <44431537+FelixPierre@users.noreply.github.com>
2020-11-19 19:55:21 +0100

    procedural walking scripts
    + height ajustement component
    + follower component
    + fast IK asset

---
+++ Assets/Scripts/HeightAdjustement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeightAdjustement : MonoBehaviour
+{
+
+    private Vector3 defaultOffset;
+
+    public float heightDelta = 1;
+    public LayerMask collisionMask;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        defaultOffset = transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Physics.queriesHitBackfaces = true;
+        RaycastHit hit;
+        Vector3 start = transform.parent.TransformPoint(defaultOffset);
+        if (Physics.Raycast(start + Vector3.up * heightDelta, Vector3.down, out hit, 2 * heightDelta, collisionMask.value)) {
+            transform.position = hit.point;
+        }
+        else {
+            transform.position = start;
+        }
+        // Physics.queriesHitBackfaces = false;
+    }
+
+    private void OnDrawGizmos() {
+        Vector3 orgn = transform.parent.TransformPoint(defaultOffset);
+        Debug.DrawLine(orgn, orgn + Vector3.up * heightDelta, Color.blue);
+        Debug.DrawLine(orgn, orgn + Vector3.down * heightDelta, Color.blue);
+        Debug.DrawLine(orgn, orgn + Vector3.right * 0.2f, Color.red);
+    }
+}
---
+++ Assets/Scripts/ProceduralWalking.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProceduralWalking : MonoBehaviour
+{
+    public float maxStepLength = 1;
+    public float verticalElevation = 1;
+
+    [SerializeField] public TargetPoint[] targetPoints;
+    public LayerMask collisionMask;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (var point in targetPoints) {
+            Debug.Log(point.targetPointDefaultPos);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveTargetPoint();
+        UpdateTarget();
+    }
+
+    void MoveTargetPoint() {
+        foreach (var point in targetPoints) {
+            RaycastHit hit;
+            Vector3 start = point.targetPoint.parent.TransformPoint(point.targetPointDefaultPos);
+            if (Physics.Raycast(start + Vector3.up * verticalElevation, Vector3.down, out hit, 2 * verticalElevation, collisionMask.value)) {
+                point.targetPoint.position = hit.point;
+            }
+            else {
+                point.targetPoint.position = start;
+            }
+        }
+    }
+
+    void UpdateTarget() {
+        foreach (var target in targetPoints) {
+            //Debug.Log(Vector3.Distance(target.boneTarget.position, target.targetPoint.position));
+            if (Vector3.Distance(target.boneTarget.position, target.targetPoint.position) > maxStepLength) {
+                target.boneTarget.position = target.targetPoint.position;
+            }
+        }
+    }
+
+    private void OnDrawGizmos() {
+        foreach (var point in targetPoints) {
+            Vector3 orgn = point.targetPoint.TransformPoint(point.targetPointDefaultPos);
+            Debug.DrawLine(orgn, orgn + Vector3.up * verticalElevation, Color.blue);
+            Debug.DrawLine(orgn, orgn + Vector3.down * verticalElevation, Color.blue);
+        }
+    }
+}
+
+
+[System.Serializable]
+public struct TargetPoint 
+{
+    [SerializeField] public Transform boneTarget;
+    [SerializeField] public Transform targetPoint;
+    [System.NonSerialized] public Vector3 targetPointDefaultPos;
+
+    public TargetPoint(Transform boneTarget, Transform targetPoint) {
+        this.boneTarget = boneTarget;
+        this.targetPoint = targetPoint;
+        targetPointDefaultPos = targetPoint.localPosition;
+    }
+}
